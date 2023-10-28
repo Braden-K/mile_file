3addc9583b789d24861b8bc973dff495c4d85d48
Braden <bkraj@icloud.com>
2023-10-28 17:33:56 -0400

    visual imp to calc

--- src/pages/RaceTimeCalculator.tsx
+++ src/pages/RaceTimeCalculator.tsx
@@ -5,6 +5,8 @@ import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useState } from "react";
 import {
+  Box,
+  Container,
   Grid,
   Paper,
   Table,
@@ -14,8 +16,16 @@ import {
   TableHead,
   TableRow,
   Typography,
+  makeStyles,
 } from "@material-ui/core";
 import { MultiButtonComponent } from "../components/MultiButtonComponent";
+
+const useStyles = makeStyles((theme) => ({
+  tableContainer: {
+    maxWidth: "600px",
+    margin: "0 auto",
+  },
+}));
 
 export const RaceTimeCalculator = () => {
   const [min, setMin] = useState("");
@@ -23,6 +33,7 @@ export const RaceTimeCalculator = () => {
   const [hourArr, setHourArr] = useState<string[]>([]);
   const [minArr, setMinArr] = useState<string[] | null>(null);
   const [remSecArr, setRemSecArr] = useState<string[]>([]);
+  const classes = useStyles();
 
   const handleMinuteChange = (event: SelectChangeEvent) => {
     setMin(event.target.value);
@@ -56,136 +67,163 @@ export const RaceTimeCalculator = () => {
   };
 
   return (
-    <Grid
-      container
-      spacing={2}
-      margin={4}
-      alignItems={"center"}
-      justifyContent={"center"}
-      display={"flex"}
-      flexDirection={"column"}
-    >
-      <Grid item xs={12}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Minutes</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={String(min)}
-            label="Minutes"
-            onChange={handleMinuteChange}
-          >
-            {Array.from(Array(7).keys()).map((i) => (
-              <MenuItem value={i + 4}>{i + 4}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Seconds</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={String(sec)}
-            label="Seconds"
-            onChange={handleSecondChange}
-          >
-            {Array.from(Array(60).keys()).map((i) => (
-              <MenuItem value={i}>{i}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <MultiButtonComponent
-          quantity={1}
-          buttonTextList={["Calculate"]}
-          clickHanlderList={[onCalculateClick]}
-          margin={4}
-        />
+    <Container>
+      <Typography>Enter a per mile pace to see race times</Typography>
+      <Grid
+        container
+        spacing={2}
+        margin={4}
+        alignItems={"center"}
+        justifyContent={"center"}
+        display={"flex"}
+        flexDirection={"column"}
+        style={{ maxWidth: "600px", margin: "0 auto" }}
+      >
+        <Grid item xs={12}>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Minutes</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={String(min)}
+              label="Minutes"
+              onChange={handleMinuteChange}
+            >
+              {Array.from(Array(7).keys()).map((i) => (
+                <MenuItem value={i + 4}>{i + 4}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Seconds</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={String(sec)}
+              label="Seconds"
+              onChange={handleSecondChange}
+            >
+              {Array.from(Array(60).keys()).map((i) => (
+                <MenuItem value={i}>{(i < 10 ? "0" : "") + i}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <MultiButtonComponent
+            quantity={1}
+            buttonTextList={["Calculate"]}
+            clickHanlderList={[onCalculateClick]}
+            margin={4}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TableContainer component={Paper} className={classes.tableContainer}>
+            <Table aria-label="collapsible table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <Typography variant="h6" style={{ fontFamily: "Poppins" }}>
+                      Distance
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography variant="h6" style={{ fontFamily: "Poppins" }}>
+                      Finishing Time
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right" width="10vw" />
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      5k
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      {minArr === null
+                        ? ""
+                        : (hourArr[0] !== "00" ? hourArr[0] + ":" : "") +
+                          (minArr != null
+                            ? minArr[0] + ":" + remSecArr[0]
+                            : "")}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      10k
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      {minArr === null
+                        ? ""
+                        : (hourArr[1] !== "00" ? hourArr[1] + ":" : "") +
+                          (minArr != null
+                            ? minArr[1] + ":" + remSecArr[1]
+                            : "")}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      10 mi
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      {minArr === null
+                        ? ""
+                        : (hourArr[2] !== "00" ? hourArr[2] + ":" : "") +
+                          (minArr != null
+                            ? minArr[2] + ":" + remSecArr[2]
+                            : "")}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      Half Marathon
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      {minArr === null
+                        ? ""
+                        : (hourArr[3] !== "00" ? hourArr[3] + ":" : "") +
+                          (minArr != null
+                            ? minArr[3] + ":" + remSecArr[3]
+                            : "")}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      Marathon
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography style={{ fontFamily: "Poppins" }}>
+                      {minArr === null
+                        ? ""
+                        : (hourArr[4] !== "00" ? hourArr[4] + ":" : "") +
+                          (minArr != null
+                            ? minArr[4] + ":" + remSecArr[4]
+                            : "")}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <TableContainer component={Paper}>
-          <Table aria-label="collapsible table">
-            <TableHead>
-              <TableRow>
-                <TableCell width="10vw" />
-                <TableCell>
-                  <Typography variant="h6" style={{ fontFamily: "Poppins" }}>
-                    Distance
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography variant="h6" style={{ fontFamily: "Poppins" }}>
-                    Finishing Time
-                  </Typography>
-                </TableCell>
-                <TableCell align="right" width="10vw" />
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>5k</Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>
-                    {(hourArr[0] !== "00" ? hourArr[0] + ":" : "") +
-                      (minArr != null ? minArr[0] + ":" + remSecArr[0] : "")}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>10k</Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>
-                    {(hourArr[1] !== "00" ? hourArr[1] + ":" : "") +
-                      (minArr != null ? minArr[1] + ":" + remSecArr[1] : "")}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>
-                    10 mi
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>
-                    {(hourArr[2] !== "00" ? hourArr[2] + ":" : "") +
-                      (minArr != null ? minArr[2] + ":" + remSecArr[2] : "")}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>
-                    Half Marathon
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>
-                    {(hourArr[3] !== "00" ? hourArr[3] + ":" : "") +
-                      (minArr != null ? minArr[3] + ":" + remSecArr[3] : "")}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>
-                    Marathon
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography style={{ fontFamily: "Poppins" }}>
-                    {(hourArr[4] !== "00" ? hourArr[4] + ":" : "") +
-                      (minArr != null ? minArr[4] + ":" + remSecArr[4] : "")}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
-    </Grid>
+    </Container>
   );
 };
